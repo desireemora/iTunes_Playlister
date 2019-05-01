f967ddc39341fddf8dfd315e7ba4878663a7c7ef
Desiree J Mora <desireemora@mail.usf.edu>
2019-05-01 12:39:42 -0400

    Update itunes_playlister.py

--- itunes_playlister.py
+++ itunes_playlister.py
@@ -2,17 +2,43 @@ from xml.dom.minidom import parse, parseString
 from xml.etree import ElementTree
 import os
 
+class track:
+    key
+    year
+    Persistent ID
+    Name
+    Artist
+    Composer
+    Album
+    Genre
+    Location
+
+class playlist:
+        songs = []
+songs = list()
 direc = "/Users/DesireeMora/Music/iTunes/iTunes_Music_Library.xml"
 
 file_name = 'iTunes_Music_Library.xml'
-full_file = os.path.abspath('/Users/DesireeMora/Music/iTunes/'+file_name)
+path_to_file = "fill this in"
+
+full_file = os.path.abspath(file_name)
 print(full_file)
 dom = ElementTree.parse(full_file) # parse an open file
 
 key = dom.findall('dict/dict/dict/')
 inte = dom.findall('dict/integer')
-count = 0
+
+count = 1
 for k in key:
     print(k.text)
-    #print(k.text,":",inte[count].text)
-    count +=1
+
+    track = track();
+    objs.append()
+    if count == 1:
+        
+
+    #Every 62 lines is a new song
+    if count == 62:
+        count = 0
+
+    count+=1
